2c6b4b58bc46cb467f268def94d1f0c3aa0697b3
Kirill Lesnykh <kirlesnykh@gmail.com>
2023-03-08 14:34:05 -0800

    challenges08 done

--- javascript/code-challenges/challenges-07.test.js
+++ javascript/code-challenges/challenges-07.test.js
@@ -147,7 +147,9 @@ You may also use other string or array methods.
 
 const splitFoods = (recipe) => {
   let result = [];
-  // Solution code here...
+  recipe.ingredients.forEach(ingredient => {
+    result.push(ingredient.slice(ingredient.indexOf(' ', 3)+1));
+  });
   return result;
 };
 
@@ -298,7 +300,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return a list of foods', () => {
     expect(splitFoods(gruffaloCrumble)).toStrictEqual(['Gruffalo', 'oats', 'brown sugar', 'flour', 'pure maple syrup', 'chopped nuts', 'baking soda', 'baking powder', 'cinnamon', 'melted butter', 'fresh water']);
   });
